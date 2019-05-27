b009f44254a642ae5102f2ad5772726850c80838
kkingoj <42208215+kkingoj@users.noreply.github.com>
2019-05-27 21:35:14 +0900

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+﻿﻿<?php
+    $host = "localhost";
+    $dbid = "db2016190606";                    // 자신의 DB ID
+    $dbpass = "kkingoj@naver.com";                  // 자신의 DB Password
+    $dbname = "db2016190606";                  // 해당 프로젝트에서 사용하는 DB명
+?>
