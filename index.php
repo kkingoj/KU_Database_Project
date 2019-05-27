fe99b0b7ef6ef2582d52e8f69a584867ae3e0f3e
kkingoj <42208215+kkingoj@users.noreply.github.com>
2019-05-27 21:34:52 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+﻿<?php include ("header.php"); ?>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://rochestb.github.io/jQuery.YoutubeBackground/src/jquery.youtubebackground.js"></script>
+    <div id="video" class="background-video"></div>
+    <style>
+        .navbar {
+            z-index:999;
+        }
+        h1, p {
+            padding: 0px 30px 0px 30px;
+            text-align:center;
+        }
+        h1 {
+            font-weight:800;
+        }
+        .container {
+            position: relative;
+            background: rgba(255, 255, 255, .9);
+        }
+        .ref {
+            font-weight:200;
+            font-size:0.5em;
+        }
+    </style>
+    <div class='container'>
+        <p align="center"><img src="images/HS.PNG" width="100%"></p>
+        <h1>은행고등학교</h1>
+        <p>은행고등학교에 오신 여러분을 진심으로 환영합니다!</br><b>은행고등학교 1학년 홈페이지입니다.</b></p>
+       
+<?php include ("footer.php"); ?>
