6b8f42edc71f38b847c80585fceadcdac853599f
kkingoj <42208215+kkingoj@users.noreply.github.com>
2019-05-27 21:35:07 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+﻿        <div class="container">
+            <div class="footer">
+	<center>Copyright (c) Young Joon Yoon</a> All rights reserved.</center>
+            </div>
+        </div>
+    </body>
+</html>
