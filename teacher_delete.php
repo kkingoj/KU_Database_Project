882019c4d5da934fd07c133ebc25beee719b9ef2
kkingoj <42208215+kkingoj@users.noreply.github.com>
2019-05-27 21:58:18 +0900

    Create teacher_delete.php

---
+++ teacher_delete.php
@@ -0,0 +1,22 @@
+﻿﻿<?php
+include "config.php";    //데이터베이스 연결 설정파일
+include "util.php";      //유틸 함수
+
+$conn = dbconnect($host,$dbid,$dbpass,$dbname);
+
+$teacher_ID = $_GET['teacher_ID'];
+
+$ret = mysqli_query($conn, "delete from teacher where teacher_ID = $teacher_ID");
+
+if(!$ret)
+{
+    msg('Query Error : '.mysqli_error($conn));
+}
+else
+{
+    s_msg ('성공적으로 삭제 되었습니다');
+    echo "<meta http-equiv='refresh' content='0;url=teacher_list.php'>";
+}
+
+?>
+
