f68d7157fbf94f871530d0f5b75c41b933c47c65
kkingoj <42208215+kkingoj@users.noreply.github.com>
2019-05-27 21:42:02 +0900

    Create student_delete.php

---
+++ student_delete.php
@@ -0,0 +1,22 @@
+﻿﻿<?php
+include "config.php";    //데이터베이스 연결 설정파일
+include "util.php";      //유틸 함수
+
+$conn = dbconnect($host,$dbid,$dbpass,$dbname);
+
+$student_ID = $_GET['student_ID'];
+
+$ret = mysqli_query($conn, "delete from student where student_ID = $student_ID");
+
+if(!$ret)
+{
+    msg('Query Error : '.mysqli_error($conn));
+}
+else
+{
+    s_msg ('성공적으로 삭제 되었습니다');
+    echo "<meta http-equiv='refresh' content='0;url=student_list.php'>";
+}
+
+?>
+
