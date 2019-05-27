ded5b3c0cb92a9376fc8e01f20c59e8ea61e1304
kkingoj <42208215+kkingoj@users.noreply.github.com>
2019-05-27 21:30:19 +0900

    Create club_list.php

---
+++ club_list.php
@@ -0,0 +1,43 @@
+﻿<?
+include "header.php";
+include "config.php";    //데이터베이스 연결 설정파일
+include "util.php";      //유틸 함수
+?>
+<div class="container">
+    <?
+    $conn = dbconnect($host, $dbid, $dbpass, $dbname);
+    $query = "select * from club";
+    $res = mysqli_query($conn, $query);
+    if (!$res) {
+         die('Query Error : ' . mysqli_error());
+    }
+    ?>
+    
+    <table class="table">
+        <thead>
+        <tr>
+            <th>No.</th>
+            <th>동아리명</th>
+            <th>관련 과목</th>
+            <th>기능</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?
+        $row_index = 1;
+        while ($row = mysqli_fetch_array($res)) {
+            echo "<tr>";
+            echo "<td>{$row_index}</td>";
+            echo "<td><a href='club_view.php?club_ID={$row['club_ID']}'>{$row['club_name']}</a></td>";
+            echo "<td>{$row['subject_name']}</td>";
+            echo "<td width='17%'>
+                <a href='club_form.php?club_ID={$row['club_ID']}'><button class='button primary small'>수정</button></a>
+                </td>";
+            echo "</tr>";
+            $row_index++;
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
+<? include("footer.php") ?>
