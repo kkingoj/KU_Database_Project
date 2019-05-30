52a849c7e830b8a03385ad1a134fd25394182330
kkingoj <42208215+kkingoj@users.noreply.github.com>
2019-05-31 00:34:42 +0900

    Update club_insert.php

--- club_insert.php
+++ club_insert.php
@@ -7,14 +7,19 @@ $conn = dbconnect($host,$dbid,$dbpass,$dbname);
 $club_name = $_POST['club_name'];
 $subject_name = $_POST['subject_name'];
 
+mysqli_query($conn, "set autocommit = 0"); // Autocommit 해제
+mysqli_query($conn, "set transaction isolation level serializable"); // Isolation Level 설정
+mysqli_query($conn, "begin"); // Begins a transaction
+
 $ret = mysqli_query($conn, "insert into club (club_name, subject_name) values('$club_name', '$subject_name')");
 if(!$ret)
 {
-	echo mysqli_error($conn);
+	mysqli_query($conn, "rollback"); // 동아리 등록 query 수행 실패 및 수행 전으로 rollback
     msg('Query Error : '.mysqli_error($conn));
 }
 else
 {
+	mysqli_query($conn, "commit"); // 동아리 등록 query 수행 성공 및  수행 내역 commit
     s_msg ('성공적으로 입력 되었습니다');
     echo "<meta http-equiv='refresh' content='0;url=club_list.php'>"; //새로 고침하고 자동적으로 Student list로 이동시킨다.
 }
